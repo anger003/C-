ad11de8f6fa9b1f4e9d62b71e9d4a22e8f12e5f3
anger003 <120928248+anger003@users.noreply.github.com>
2022-12-18 22:48:32 +0800

    更新 源.cpp

--- Project1/源.cpp
+++ Project1/源.cpp
@@ -1,8 +1,32 @@
+// morechar.cpp -- the char type and int type contrasted
 #include <iostream>
-#include <Windows.h>
+int main()
+{
+    using namespace std;
+    char ch = 'M';           // assign ASCII code for M to ch
+    int i = ch;              // store same code in an int
+    cout << "The ASCII code for " << ch << " is " << i << endl;
 
+<<<<<<< HEAD
 int main() {
+	std::cout << "hello world" << std::endl;
 	std::cout << "hello world" << std::endl;
 	system("pause");
 	return 0;
+=======
+    cout << "Add one to the character code:" << endl;
+    ch = ch + 1;             // change character code in ch
+    i = ch;                  // save new character code in i
+    cout << "The ASCII code for " << ch << " is " << i << endl;
+
+    // using the cout.put() member function to display a char
+    cout << "Displaying char ch using cout.put(ch): ";
+    cout.put(ch);
+
+    // using cout.put() to display a char constant
+    cout.put('!');
+
+    cout << endl << "Done" << endl;
+    return 0;
+>>>>>>> parent of 0b81a46 (更新 源.cpp)
 }
